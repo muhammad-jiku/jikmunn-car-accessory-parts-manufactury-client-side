5332145f48de6b228b9b4fe60a44271507fc3d75
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-23 12:29:16 +0600

    first commit in client side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './Pages/Authentication/SignIn/SignIn';
+import SignUp from './Pages/Authentication/SignUp/SignUp';
+import Blog from './Pages/Blog/Blog';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-primary text-5xl text-center my-6">
+        This is Homepage
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
