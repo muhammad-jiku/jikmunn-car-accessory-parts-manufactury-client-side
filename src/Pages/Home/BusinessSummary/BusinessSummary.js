f729ad67f4fa8b10f3c986a0b281d64f1605795e
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-24 13:29:57 +0600

    three car parts displaying in the homepage

--- src/Pages/Home/BusinessSummary/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary/BusinessSummary.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 function BusinessSummary() {
   return (
-    <div class="stats stats-vertical lg:stats-horizontal shadow w-full">
-      <div class="stat">
-        <div class="stat-title">Downloads</div>
-        <div class="stat-value">31K</div>
-        <div class="stat-desc">Jan 1st - Feb 1st</div>
+    <div className="stats stats-vertical lg:stats-horizontal shadow w-full">
+      <div className="stat">
+        <div className="stat-title">Downloads</div>
+        <div className="stat-value">31K</div>
+        <div className="stat-desc">Jan 1st - Feb 1st</div>
       </div>
 
-      <div class="stat">
-        <div class="stat-title">New Users</div>
-        <div class="stat-value">4,200</div>
-        <div class="stat-desc">↗︎ 400 (22%)</div>
+      <div className="stat">
+        <div className="stat-title">New Users</div>
+        <div className="stat-value">4,200</div>
+        <div className="stat-desc">↗︎ 400 (22%)</div>
       </div>
 
-      <div class="stat">
-        <div class="stat-title">New Registers</div>
-        <div class="stat-value">1,200</div>
-        <div class="stat-desc">↘︎ 90 (14%)</div>
+      <div className="stat">
+        <div className="stat-title">New Registers</div>
+        <div className="stat-value">1,200</div>
+        <div className="stat-desc">↘︎ 90 (14%)</div>
       </div>
     </div>
   );
