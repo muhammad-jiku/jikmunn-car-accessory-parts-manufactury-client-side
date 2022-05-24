25206d41597ffd406515787a5b210c69c26f6399
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-25 00:24:50 +0600

    social login added

--- src/Pages/Authentication/SocialSignIn/SocialSignIn.js
+++ src/Pages/Authentication/SocialSignIn/SocialSignIn.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import googleLogo from '../../../Images/google.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 function SocialSignIn() {
-  return <div>SocialSignIn</div>;
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  // if (error) {
+  //   return (
+  //     <div>
+  //       <p>Error: {error.message}</p>
+  //     </div>
+  //   );
+  // }
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
+  // if (user) {
+  //   return (
+  //     <div>
+  //       <p>Signed In User: {user.email}</p>
+  //     </div>
+  //   );
+  // }
+
+  const handleGoogleLogin = async () => {
+    await signInWithGoogle();
+  };
+  return (
+    <div>
+      <button
+        className="btn btn-outline btn-primary w-full"
+        onClick={handleGoogleLogin}
+      >
+        Continue with <img src={googleLogo} alt="" className="ml-2" />{' '}
+      </button>
+    </div>
+  );
 }
 
 export default SocialSignIn;
