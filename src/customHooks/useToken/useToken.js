ae6ab848d9843f3fb4c2124e0e3f1aba8612ba8d
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-25 16:00:58 +0600

    created custom hooks for token

---
+++ src/customHooks/useToken/useToken.js
@@ -0,0 +1,32 @@
+const { useState, useEffect } = require('react');
+
+const useToken = (user) => {
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    const email = user?.user?.email;
+    const currentUser = { email: email };
+    if (email) {
+      fetch(`http://localhost:5000/user/${email}`, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(currentUser),
+      })
+        .then((res) => {
+          console.log('res ', res);
+          return res.json();
+        })
+        .then((data) => {
+          console.log('data inside user token ', data);
+          const accessToken = data?.accessToken;
+          localStorage?.setItem('accessToken', accessToken);
+          setToken(accessToken);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+  return [token];
+};
+
+export default useToken;
