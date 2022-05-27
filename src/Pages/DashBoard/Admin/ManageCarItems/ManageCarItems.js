cdc570554cc5304aace7c38c8a7314f0fa4d9edf
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-27 11:20:53 +0600

    fetching caritems for admin is added

--- src/Pages/DashBoard/Admin/ManageCarItems/ManageCarItems.js
+++ src/Pages/DashBoard/Admin/ManageCarItems/ManageCarItems.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ManageCarItems() {
-  return <div>ManageCarItems</div>;
+  const [carItems, setCarItems] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/car-parts')
+      .then((res) => res.json())
+      .then((data) => setCarItems(data))
+      .catch((err) => console.log(err));
+  });
+  return (
+    <div>
+      ManageCarItems
+      {console.log(carItems)}
+    </div>
+  );
 }
 
 export default ManageCarItems;
