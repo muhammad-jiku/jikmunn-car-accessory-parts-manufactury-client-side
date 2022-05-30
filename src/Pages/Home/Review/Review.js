4567830959a05853fd7980d6a43e398a291927c2
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-30 17:18:23 +0600

    review section modified

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -19,49 +19,52 @@ function Review() {
 
   return (
     <div className="container mx-auto my-6 grid grid-cols-1 gap-4 lg:grid-cols-3">
-      {reviews?.map((review) => (
-        <div
-          className="card bg-base-100 border-2 border-purple-700"
-          key={review?._id}
-        >
-          {/* <SwiperSlide> */}
-          <div className="card-body font-bold">
-            <div className="card-actions justify-start">
-              <div className="avatar">
-                <div className="w-12 lg:w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                  <img src={review?.img} alt={review?.displayName} />
+      {reviews
+        ?.slice(0)
+        ?.reverse()
+        ?.map((review) => (
+          <div
+            className="card bg-base-100 border-2 border-purple-700"
+            key={review?._id}
+          >
+            {/* <SwiperSlide> */}
+            <div className="card-body font-bold">
+              <div className="card-actions justify-start">
+                <div className="avatar">
+                  <div className="w-12 lg:w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img src={review?.img} alt={review?.displayName} />
+                  </div>
+                </div>
+                <div className="flex flex-col items-start justify-start font-semibold px-2">
+                  <h1 className="text-lg">{review?.displayName}</h1>
+                  <h1 className="text-xl flex items-center">
+                    {review?.rating}{' '}
+                    <span>
+                      <FontAwesomeIcon
+                        icon={faStar}
+                        // className="text-primary"
+                        style={{ color: 'yellow' }}
+                        size="xl"
+                      />
+                    </span>
+                  </h1>
                 </div>
               </div>
-              <div className="flex flex-col items-start justify-start font-semibold px-2">
-                <h1 className="text-lg">{review?.displayName}</h1>
-                <h1 className="text-xl flex items-center">
-                  {review?.rating}{' '}
-                  <span>
-                    <FontAwesomeIcon
-                      icon={faStar}
-                      // className="text-primary"
-                      style={{ color: 'yellow' }}
-                      size="xl"
-                    />
-                  </span>
-                </h1>
-              </div>
+              <FontAwesomeIcon
+                icon={faQuoteLeft}
+                className="text-primary"
+                size="xl"
+              />
+              <p>{review?.review}</p>
+              <FontAwesomeIcon
+                icon={faQuoteRight}
+                className="text-primary"
+                size="xl"
+              />
             </div>
-            <FontAwesomeIcon
-              icon={faQuoteLeft}
-              className="text-primary"
-              size="xl"
-            />
-            <p>{review?.review}</p>
-            <FontAwesomeIcon
-              icon={faQuoteRight}
-              className="text-primary"
-              size="xl"
-            />
+            {/* </SwiperSlide> */}
           </div>
-          {/* </SwiperSlide> */}
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
